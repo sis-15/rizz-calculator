3bc0a4b70d46533fea35e6503eab9ecf1b100c5b
sis-15 <85040035+sis-15@users.noreply.github.com>
2026-09-08 01:21:05 -0400

    Fixed LaTeX again

--- src/App.jsx
+++ src/App.jsx
@@ -50,14 +50,14 @@ export default function App() {
   // LaTeX string representation with live variables inserted
 const rawLatex = `
 \\begin{aligned}
-P(\\text{Success}) &= \\frac{1}{1 + e^{-y}} \\[8pt]
+P(\\text{Success}) &= \\frac{1}{1 + e^{-y}} \\\\[8pt]
 y &= \\left[ \\left( P(L) - \\frac{\\varnothing}{1 + 0.5a} \\right) \\cdot \\mu \\cdot (1.25)^a - 1 \\right]
 \\end{aligned}
-`; 
+`;
 
 const liveLatex = `
 \\begin{aligned}
-P(\\text{Success}) &= \\frac{1}{1 + e^{-y}} \\[8pt]
+P(\\text{Success}) &= \\frac{1}{1 + e^{-y}} \\\\[8pt]
 y &= \\left[ \\left( ${plVal} - \\frac{${pNotLVal}}{1 + 0.5(${aVal})} \\right) \\cdot ${muVal} \\cdot (1.25)^{${aVal}} - 1 \\right] = ${exponentTerm.toFixed(2)}
 \\end{aligned}
 `;
